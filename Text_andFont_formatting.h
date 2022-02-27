21ed1a482e97dcf510d9b198102ea12e4d4e0883
Tarun Dev <62273965+tarundev-x@users.noreply.github.com>
2022-02-27 23:43:03 +0530

    Create Text_andFont_formatting.h

---
+++ Text_andFont_formatting.h
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Text and Font formatting in CSS</title>
+    <style>
+    div{
+        font-size: 50px;
+        color: red;
+        font-family:cursive;
+        font-weight: bold;
+        font-style: oblique;
+    }
+    #second{
+        font-size: 50%;
+        color : rgb(255,0,0);
+        font-family: "Times New Roman";
+        font-weight: 900;
+        font-style: italic;
+    }
+    </style>
+</head>
+<body>
+    <div>
+        <p id="first">This is first paragraph</p>
+        <p id="second">This is second paragraph</p>
+    </div>
+</body>
+</html>
